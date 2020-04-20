778085bb40c62e83b2dcd29bde46c247ee852cec
aelia <avishome@gmail.com>
2020-04-20 10:48:53 +0300

    remove useless dependenty

--- ClassLibrary1/Report.cs
+++ ClassLibrary1/Report.cs
@@ -1,10 +1,10 @@
-﻿using PropertyChanged;
+﻿
 using System;
 using System.ComponentModel;
 
 namespace Entities
 {
-    public class Report : INotifyPropertyChanged
+    public class Report 
     {
         
         public string Id { get; set; }
